fc56a5f32f27651ef5510843c86e99de35085e2c
User <User@DESKTOP-LDD4AU9>
2024-05-20 18:21:06 +0900

    송금 table & 송금에 관한 것 모든 것

--- BankProject/src/main/java/com/example/demo/controller/AccountController.java
+++ BankProject/src/main/java/com/example/demo/controller/AccountController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +10,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.jpa.AccountRepo;
 import com.example.demo.vo.AccountVO;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class AccountController {
@@ -30,6 +35,29 @@ public class AccountController {
 		
 		mav.addObject("result","계좌 개설 완료");
 		mav.setViewName("account/accountComplete");
+		return mav;
+	}
+	
+	@RequestMapping(value="remitMoney")
+	public ModelAndView remitMoney(HttpServletRequest request) {
+		
+		String id = request.getParameter("id");
+		System.out.println(id);
+		List<String> accountnum = new ArrayList<>();
+		
+		accountnum = accountrepo.selectAccount(id);
+		System.out.println(accountnum);
+		
+		mav.addObject("accountnum", accountnum);
+		mav.setViewName("account/remit");
+		return mav;
+	}
+	
+	@RequestMapping(value="remitComplete")
+	public ModelAndView remitComplete(HttpServletRequest request) {
+		
+		String account = request.getParameter("account");
+		System.out.println(account);
 		return mav;
 	}
 }
--- BankProject/src/main/java/com/example/demo/jpa/AccountRepo.java
+++ BankProject/src/main/java/com/example/demo/jpa/AccountRepo.java
@@ -1,9 +1,17 @@
 package com.example.demo.jpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.vo.AccountVO;
 
 public interface AccountRepo extends JpaRepository<AccountVO, Integer>{
-
+	@Query(value="insert into account(total) values(:na)", nativeQuery = true)
+	public void insertAccount(@Param(value="na") String money);
+	
+	@Query(value="select account_num from account where id=:na", nativeQuery = true)
+	public List<String> selectAccount(@Param(value="na") String id);
 }
